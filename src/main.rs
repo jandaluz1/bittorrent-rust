a5e89a70411cbf69f13350816f2c8fcd0059fca8
Joe Andaluz <jandaluz1@gmail.com>
2024-06-28 16:17:12 -0400

    reading file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-    println!("Entered command: {}",&command)
+    let file_path = &args[1];
+    let torrent = fs::read_to_string(file_path).expect("Should be able to read file.");
+
+    println!("{}", torrent);
 }
